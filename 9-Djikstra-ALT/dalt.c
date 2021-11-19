d3e23b87c97643f3f8533539e33eeff6a7da2988
Vaernion <vaporz@gmail.com>
2021-11-19 01:45:20 +0100

    partially working djikstra, write path to csv

--- 9-Djikstra-ALT/dalt.c
+++ 9-Djikstra-ALT/dalt.c
@@ -7,15 +7,15 @@
 #define infinity 1000000000
 
 // --- needed ---
-// heap priority queue - adjust max heap functions for min heap
-// djikstra
+// X heap priority queue - adjust max heap functions for min heap
+// djikstra - bugged? too long distance
 // ALT
 // preprocessing landmark distances - compact binary file
 //      metadata at start of file? (nr of landmarks & landmark ids)
 //      x ints per node, use zero index as node nr
 //      write preprocessed file
 //      progress indicator (update every hundred/thousand nodes?)
-// read node & edge files - fscanf fast enough?
+// X read node & edge files - fscanf fast enough?
 // write coordinates of routes as lat,long
 // time djikstra & ALT
 // CLI arguments for preprocessing, or hardcode and check if preprocessed file exists?
@@ -24,10 +24,12 @@
 
 typedef struct NodeStruct
 {
-    struct EdgeStruct *edgeHead;
+    int nr;
     int distance;
+    bool checked;
     double lat;
     double lon;
+    struct EdgeStruct *edgeHead;
     struct NodeStruct *previous;
 } Node;
 
@@ -47,10 +49,10 @@ typedef struct GraphStruct
 
 typedef struct RouteStruct
 {
-    int source;
+    int start;
     int destination;
-    int i;
-    Node *path;
+    int numNodes;
+    Node **path;
 } Route;
 
 typedef struct HeapStruct
@@ -83,7 +85,7 @@ void heapPrioUp(Heap *heap, int i, int p)
 {
     int f;
     heap->nodes[i] += p;
-    while (i && heap->nodes[i] > heap->nodes[f = heapOver(i)])
+    while (i && heap->nodes[i] < heap->nodes[f = heapOver(i)])
     {
         heapSwap(&heap->nodes[i], &heap->nodes[f]);
         i = f;
@@ -92,22 +94,11 @@ void heapPrioUp(Heap *heap, int i, int p)
 
 void heapInsert(Heap *heap, int x)
 {
-    heap->length++;
-    printf("Starting heapInsert x:%d\n", x);
-    // if (heap->nodes == NULL)
-    // {
-    //     heap->nodes = malloc(heap->length * sizeof(int));
-    //     printf("malloc'd\n");
-    // }
-    // else
-    // {
-    //     heap->nodes = realloc(heap->nodes, heap->length * sizeof(int));
-    //     printf("realloc'd length: %d\n", heap->length);
-    // }
-
+    // printf("heapInsert x:%d heap->length: %i\n", x, heap->length);
     int i = heap->length;
     heap->nodes[i] = x;
     heapPrioUp(heap, i, 0);
+    heap->length++;
 }
 
 void heapFix(Heap *heap, int i)
@@ -116,9 +107,9 @@ void heapFix(Heap *heap, int i)
     if (m < heap->length)
     {
         int h = m + 1;
-        if (h < heap->length && heap->nodes[h] > heap->nodes[m])
+        if (h < heap->length && heap->nodes[h] < heap->nodes[m])
             m = h;
-        if (heap->nodes[m] > heap->nodes[i])
+        if (heap->nodes[m] < heap->nodes[i])
         {
             heapSwap(heap->nodes + i, heap->nodes + m);
             heapFix(heap, m);
@@ -126,12 +117,12 @@ void heapFix(Heap *heap, int i)
     }
 }
 
-void heapCreate(Heap *heap)
-{
-    int i = heap->length / 2;
-    while (i--)
-        heapFix(heap, i);
-}
+// void heapCreate(Heap *heap)
+// {
+//     int i = heap->length / 2;
+//     while (i--)
+//         heapFix(heap, i);
+// }
 
 int heapGetMin(Heap *heap)
 {
@@ -159,6 +150,20 @@ void initDistances(Graph *graph, int start)
     graph->nodes[start].distance = 0;
 }
 
+void resetNodes(Graph *graph, Route *route, int start)
+{
+    initDistances(graph, start);
+    for (int i = 0; i < graph->n; i++)
+    {
+        graph->nodes[i].checked = false;
+        graph->nodes[i].previous = NULL;
+    }
+
+    route->numNodes = 0;
+    free(route->path);
+    route->path = NULL;
+}
+
 Graph *readGraph(char nodeFile[], char edgeFile[], bool reverseGraph)
 {
     FILE *fpNodes = fopen(nodeFile, "r");
@@ -183,15 +188,23 @@ Graph *readGraph(char nodeFile[], char edgeFile[], bool reverseGraph)
 
     graph->nodes = calloc(graph->n, sizeof(Node));
 
-    for (int i = 0; i < graph->k; i++)
+    // read nodes
+    for (int i = 0; i < graph->n; i++)
     {
         int nr;
         double lat, lon;
         fscanf(fpNodes, "%i %lf %lf\n", &nr, &lat, &lon);
-        graph->nodes[nr].lat = lat;
-        graph->nodes[nr].lon = lon;
+        Node *node = &graph->nodes[nr];
+        node->nr = nr;
+        node->lat = lat;
+        node->lon = lon;
+
+        // graph->nodes[nr].nr = nr;
+        // graph->nodes[nr].lat = lat;
+        // graph->nodes[nr].lon = lon;
     }
 
+    // read edges
     for (int i = 0; i < graph->k; i++)
     {
         int from, to, carTime, length, speedLimit;
@@ -212,8 +225,199 @@ Graph *readGraph(char nodeFile[], char edgeFile[], bool reverseGraph)
     return graph;
 }
 
+Route *initRoute(int start, int destination)
+{
+    Route *route = calloc(1, sizeof(Route));
+    route->start = start;
+    route->destination = destination;
+    return route;
+}
+
+Heap *initHeap(int n)
+{
+    Heap *heap = calloc(1, sizeof(Heap));
+    heap->nodes = calloc(n, sizeof(int));
+    return heap;
+}
+
+void printHeap(Heap *heap, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%i ", heap->nodes[i]);
+    }
+    printf("\n");
+}
+
+void testHeap()
+{
+    int n = 7;
+    int abc[] = {8, 52, 2, 3, 1, 8, 77};
+    Heap *heap = initHeap(n);
+
+    // insert test data
+    for (int i = 0; i < n; i++)
+    {
+        // print heap contents on one line
+        printf("HEAP INSERT:%i\n", abc[i]);
+        printHeap(heap, n);
+
+        heapInsert(heap, abc[i]);
+        printf("%i i:%i heapLength: %i\n", abc[i], i, heap->length);
+
+        printHeap(heap, n);
+        printf("\n");
+    }
+
+    // retrieve test data in min sorted order (priority queue)
+    while (heap->length > 0)
+    {
+        printf("HEAP GETMIN\n");
+        printHeap(heap, n);
+
+        int min = heapGetMin(heap);
+        printf("%i heapLength: %i\n", min, heap->length);
+
+        printHeap(heap, n);
+        printf("\n");
+    }
+}
+
+void printDrivingTime(int carTime)
+{
+    const int secondsInHour = 3600;
+    int rawSeconds = carTime / 100;
+    int h = rawSeconds / secondsInHour;
+    int remainingSeconds = rawSeconds - (h * secondsInHour);
+    int m = remainingSeconds / 60;
+    int minutesAsSeconds = m * 60;
+    int s = remainingSeconds - minutesAsSeconds;
+
+    printf("%d:%02d:%02d", h, m, s);
+}
+
+void findPath(Graph *graph, Route *route)
+{
+    printf("\n---findPath---\n");
+    int pathLength = 0;
+
+    for (Node *node = &graph->nodes[route->destination];
+         node != NULL; node = node->previous)
+    {
+        // printf("%i ", node->nr);
+        pathLength++;
+    }
+    printf("pathLength: %i\n", pathLength);
+
+    route->numNodes = pathLength;
+    route->path = calloc(route->numNodes, sizeof(Node *));
+
+    Node *node = &graph->nodes[route->destination];
+    for (int i = route->numNodes - 1; i >= 0; i--)
+    {
+        route->path[i] = node;
+        node = node->previous;
+    }
+
+    // path in correct order
+    for (int i = 0; i < route->numNodes; i++)
+    {
+        // printf("%i ", route->path[i]->nr);
+    }
+    printf("\n");
+}
+
+void writePath(Route *route, char outFile[])
+{
+    FILE *fpOut = fopen(outFile, "w");
+    if (fpOut == NULL)
+    {
+        perror("Error while opening outfile");
+        exit(1);
+    }
+
+    char csvColumns[] = "nr,node,latitude,longitude\n";
+    fwrite(csvColumns, strlen(csvColumns), 1, fpOut);
+    // up to 3 chars for negative, 1 dot, 7 decimals, and 1 string termination
+    const int coordLength = 12;
+
+    for (int i = 0; i < route->numNodes; i++)
+    {
+        char pathNr[10];
+        char nodeNr[30];
+        char lat[coordLength], lon[coordLength];
+        snprintf(pathNr, 10, "%i", i + 1);
+        snprintf(nodeNr, 30, "%i", route->path[i]->nr);
+        snprintf(lat, coordLength, "%.8f", route->path[i]->lat);
+        snprintf(lon, coordLength, "%.8f", route->path[i]->lon);
+        // printf("node: %i lat: %s lon: %s\n", route->path[i]->nr, lat, lon);
+
+        fwrite(pathNr, sizeof(char), strlen(pathNr), fpOut);
+        fwrite(",", sizeof(char), 1, fpOut);
+        fwrite(nodeNr, sizeof(char), strlen(nodeNr), fpOut);
+        fwrite(",", sizeof(char), 1, fpOut);
+        fwrite(lat, sizeof(char), strlen(lat), fpOut);
+        fwrite(",", sizeof(char), 1, fpOut);
+        fwrite(lon, sizeof(char), strlen(lat), fpOut);
+        fwrite("\n", sizeof(char), 1, fpOut);
+    }
+}
+
 void djikstra(Graph *graph, Route *route, bool stopEarly)
 {
+    printf("\n--- djikstra from: %i to: %i---\n", route->start, route->destination);
+    Heap *heap = initHeap(graph->n);
+    heapInsert(heap, route->start);
+    int checked = 0;
+
+    while (heap->length > 0)
+    {
+        int nodeNr = heapGetMin(heap);
+        Node *node = &graph->nodes[nodeNr];
+        node->checked = true;
+        checked++;
+
+        if (node->nr == route->start)
+        {
+            printf("start\n");
+        }
+
+        // printf("nodeNr: %i node.nr: %i lat: %i lon: %i dist: %i\n",
+        //        nodeNr, node.nr, node.lat, node.lon, node.distance);
+
+        if (stopEarly && node->nr == route->destination)
+        {
+            printf("found target: %i dist: %i carTime (h:m:s) ",
+                   node->nr, node->distance);
+            printDrivingTime(node->distance);
+            printf("\n");
+            break;
+        }
+
+        int edges = 0;
+
+        // check all neighbors and update distances
+        for (Edge *edge = graph->nodes[nodeNr].edgeHead; edge != NULL; edge = edge->next)
+        {
+            edges++;
+            if (edge->to->nr == route->start)
+            {
+                printf("back at start, checked: %i, edges: %i\n", checked, edges);
+            }
+
+            Node *neighbor = edge->to;
+            int newDistance = node->distance + edge->weight;
+
+            if (!neighbor->checked && newDistance < neighbor->distance)
+            {
+                neighbor->distance = newDistance;
+                neighbor->previous = node;
+                heapInsert(heap, edge->to->nr);
+            }
+        }
+    }
+    printf("--- djikstra done, checked %i nodes\n", checked);
+    free(heap);
 }
 
 void preProcess()
@@ -222,49 +426,42 @@ void preProcess()
 
 void test(char nodeFile[], char edgeFile[], int from, int to)
 {
+    printf("---test--- nodeFile: %s edgeFile: %s\n", nodeFile, edgeFile);
     Graph *graph = readGraph(nodeFile, edgeFile, false);
     initDistances(graph, from);
+    Route *route = initRoute(from, to);
+    char outFile[] = "path.csv";
 
-    int n = 7;
-    int abc[] = {82, 21, 35, 11, 59, 111, 62};
+    // testHeap();
 
-    Heap *heap = calloc(1, sizeof(Heap));
-    heap->nodes = calloc(graph->n, sizeof(int));
-    // heap->length = n;
-    for (int i = 0; i < n; i++)
+    // Node start = graph->nodes[from];
+    // printf("\nstart:%i nr:%i dist:%i lat:%f lon:%f\n",
+    //        from, start.nr, start.distance, start.lat, start.lon);
+
+    // printf("--- first 5 nodes ---\n");
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     Node node = graph->nodes[i];
+    //     printf("node:%i dist:%i lat:%f lon:%f\n",
+    //            i, node.distance, node.lat, node.lon);
+    // }
+    // printf("--- last 5 nodes ---\n");
+    // for (int i = graph->n - 5; i < graph->n; i++)
+    // {
+    //     Node node = graph->nodes[i];
+    //     printf("node:%i dist:%i lat:%f lon:%f\n",
+    //            i, node.distance, node.lat, node.lon);
+    // }
+
+    djikstra(graph, route, true);
+    findPath(graph, route);
+    writePath(route, outFile);
+
+    if (strcmp(nodeFile, "ice/noder.txt") == 0)
     {
-        // heap->nodes[i] = abc[i];
-        heapInsert(heap, abc[i]);
-        printf("inserted %i\n", i + 1);
+        resetNodes(graph, route, from);
+        djikstra(graph, route, false);
     }
-
-    for (int i = 0; i < n; i++)
-    {
-        // heap->nodes[i] = abc[i];
-        int min = heapGetMin(heap);
-        printf("heapLength: %i i:%i %d\n", heap->length, i, min);
-    }
-
-    Node start = graph->nodes[from];
-    printf("\nstart:%i dist:%i lat:%f lon:%f\n",
-           from, start.distance, start.lat, start.lon);
-
-    printf("--- first 5 nodes ---\n");
-    for (int i = 0; i < 5; i++)
-    {
-        Node node = graph->nodes[i];
-        printf("node:%i dist:%i lat:%f lon:%f\n",
-               i, node.distance, node.lat, node.lon);
-    }
-    printf("--- last 5 nodes ---\n");
-    for (int i = graph->n - 5; i < graph->n; i++)
-    {
-        Node node = graph->nodes[i];
-        printf("node:%i dist:%i lat:%f lon:%f\n",
-               i, node.distance, node.lat, node.lon);
-    }
-
-    heapCreate(heap);
 }
 
 void routeTerminal(char preFile[])
@@ -314,8 +511,17 @@ int main(int argc, char *argv[])
             char nodeFile[] = "ice/noder.txt";
             char edgeFile[] = "ice/kanter.txt";
             int reykjavik = 30979;
-            int skogar = 12220;
-            test(nodeFile, edgeFile, reykjavik, skogar);
+            int selfoss = 107046;
+            test(nodeFile, edgeFile, reykjavik, selfoss);
+            return 0;
+        }
+        if (strcmp(argv[1], "tr2") == 0)
+        {
+            char nodeFile[] = "../../../norden/noder.txt";
+            char edgeFile[] = "../../../norden/kanter.txt";
+            int meraaker = 6579983;
+            int stjordal = 1693246;
+            test(nodeFile, edgeFile, meraaker, stjordal);
             return 0;
         }
         if (strcmp(argv[1], "tp") == 0)
